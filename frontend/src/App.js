5867b4175d7f481f9c882892904135dc75735183
Purvesh Makode <makodepurvesh@gmail.com>
2020-04-27 16:11:13 +0530

    Passed values through the react components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import { render } from "react-dom";
-// import Post from './Post'
+import Post from './Post'
 
 import './static/mainnew.css'
 
@@ -52,38 +52,8 @@ class App extends Component {
       <ul>
         {this.state.data.map(post => {
           return (
-            //  <Post post={post} />
-            <div class="post">
+             <Post post={ post } />
             
-              <h1 class="ui header">{post.name}</h1>
-              <h2 class="ui header">{post.email}</h2>
-              <h3 class="ui header">{post.message}</h3>
-
-
-              <div class="ui animated button" tabindex="0" bis_skin_checked="1">
-                <div class="visible content" bis_skin_checked="1">Next</div>
-                <div class="hidden content" bis_skin_checked="1">
-                  <i class="right arrow icon"></i>
-                </div>
-              </div>
-              <div class="ui vertical animated button" tabindex="0" bis_skin_checked="1">
-                <div class="hidden content" bis_skin_checked="1">Shop</div>
-                <div class="visible content" bis_skin_checked="1">
-                  <i class="shop icon"></i>
-                </div>
-              </div>
-              <div class="ui animated fade button" tabindex="0" bis_skin_checked="1">
-                <div class="visible content" bis_skin_checked="1">Sign-up for a Pro account</div>
-                <div class="hidden content" bis_skin_checked="1">
-                  $12.99 a month
-                </div>
-              </div>
-
-              
-
-            </div>
-
-
           );
         })}
       </ul>
