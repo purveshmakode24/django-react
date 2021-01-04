9fa2eea6170463035d42cd770f7fbc682aa7efc3
Purvesh Makode <makodepurvesh@gmail.com>
2021-01-04 23:49:29 +0530

    Update Post.js

--- frontend/src/Components/Post.js
+++ frontend/src/Components/Post.js
@@ -5,25 +5,17 @@ import UpdateForm from "./UpdateForm";
 import DeleteForm from "./DeleteForm";
 
 // export default class Post extends Component {
-
 //   render(){
 //     return (
 //       <div className="post">
-
 //             <h1 className="ui header">{this.props.post.name}</h1>
 //             <h2 className="ui header">{this.props.post.email}</h2>
 //             <h3 className="ui header">{this.props.post.message}</h3>
-
 //             <UpdateForm post={this.props.post} />
-
-
 //       </div>
-
 //   );
 //   }
-
 // }
-
 
 
 export default function Post(props) {
